2b864564a3951e02b4e5c6261eff26a1e84487c1
sberchenko <sberchenko@itascacg.com>
2021-02-16 15:47:57 -0600

    deleting files

--- compressor.py
+++ compressor.py
@@ -5,6 +5,7 @@ import subprocess
 exe = "C:\\Program Files\\7-Zip\\7z.exe"        # 7-Zip executable path
 directory = 'test'                              # Directory which will be searched for files to compress
 file_extensions = ['.sav', '.3dsav', '.f3sav']  # File extensions that should be compressed
+delete = True                                   # Whether to delete files after
 
 
 def compress(file):
@@ -13,25 +14,13 @@ def compress(file):
     :param file: a string path to the file
     """
     target = file[:file.rfind('.')] + '.7z'
-    subprocess.call(exe + " a -t7z " + target + " " + file)
+    subprocess.call(f'"{exe}" a {"-sdel" if delete else ""} -tzip "{target}" "{file}"')
 
 
-def compress_all(files, target):
+def compress_dir(directory):
     """
-    Compresses all of the inputted files into a single .7z
-    :param files: an array of string paths to the files
-    :param target: path to save directory
-    """
-    file_str = ""
-    for file in files: file_str += file + " "
-    subprocess.call(exe + " a -t7z " + target + " " + file_str)
-
-
-
-def main():
-    """
-    Compresses all files with the specified file extensions in the specified
-       directory.
+    Recursively compresses all of the specified files in the inputted directory
+    :param directory: dir to be compressed
     """
     files = os.listdir(directory)
     files = [directory + '/' + file for file in files]
@@ -39,15 +28,22 @@ def main():
 
     # Zipping individually
     for file in files:
-        # Assert file hasn't been zipped already
-        if open(file, 'rb').read(2) != b'7z':
-            for extension in file_extensions:
-                if extension in file:
-                    compress(file)
-                    files_compressable.append(file)
+        # Recurse if file is a directory
+        if os.path.isdir(f"{directory}/{file}"):
+            compress_dir(f"{directory}/{file}")
+        # Check whether file should be compressed
+        for extension in file_extensions:
+            if extension in file:
+                compress(file)
+                files_compressable.append(file)
 
-    # Zipping collectively
-    compress_all(files_compressable, directory + '/testAll.7z')
+
+def main():
+    """
+    Compresses all files with the specified file extensions in the specified
+       directory.
+    """
+    compress_dir(directory)
 
 
 if __name__ == '__main__':
